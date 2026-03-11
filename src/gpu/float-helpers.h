ebe0ac96e4a3952fce0539aa0e9b089b7c54c903
Gabriel Tsai <gabrielhtsai@gmail.com>
2026-03-11 04:52:20 -0700

    float helpers for NV pipeline

---
+++ src/gpu/float-helpers.h
@@ -0,0 +1,29 @@
+#ifndef FLOAT_HELPERS_H
+#define FLOAT_HELPERS_H
+#include "rpi.h"
+#include <stdint.h>
+
+// ex: 
+static inline int16_t float_to_fixed12_4(float f) { 
+    return (int16_t)(f * 16.0f);
+}
+
+static inline int16_t float_to_fixed12_4_round(float f) { 
+    return (int16_t) (f * 16.0f + (f >= 0.0f ? 0.5f : -0.5f));
+}
+
+static inline float fixed12_4_to_float(int16_t x) { 
+    return (float)x / 16.0f;
+}
+
+// NV pipeline stores x_s in 16 LSBs, y_s in 16 MSBs
+static inline uint32_t pack_xs_ys_fixed12_4(int16_t xs, int16_t ys) { 
+    return (uint32_t)(ys << 16 | xs);
+}
+
+static inline void unpack_xs_ys_fixed12_4(uint32_t packed, int16_t *xs, int16_t *ys) { 
+    *xs = (int16_t)(packed & 0xFFFF);
+    *ys = (int16_t)(packed >> 16);
+}
+
+#endif
--- src/tests/test_float.c
+++ src/tests/test_float.c
@@ -1,6 +1,27 @@
 #include "rpi.h"
+#include <stdint.h>
+#include <math.h>
+#include "float-helpers.h"
 
 void notmain(void) {
-    double x = 3.141592;
-    printk("hello from pi=%f float!!\n", x);
+    float x = 3.23f;
+    float y = 4.73f; // 
+    int16_t xs = float_to_fixed12_4(x);
+    int16_t ys = float_to_fixed12_4(y);
+    int16_t xs_round = float_to_fixed12_4_round(x);
+    int16_t ys_round = float_to_fixed12_4_round(y);
+    
+    // using rounding gets us closer to original float than truncation
+    // 12.4 values are spaced 1/16 apart. truncation always picks the lower 
+    // 1/16 value. rounding picks the nearest 1/16 value.
+    assert(fabs(fixed12_4_to_float(xs_round) - x) < fabs(fixed12_4_to_float(xs) - x));
+    assert(fabs(fixed12_4_to_float(ys_round) - y) < fabs(fixed12_4_to_float(ys) - y));
+
+    printk("packed: %x\n", pack_xs_ys_fixed12_4(x, y));
+    int16_t xs_out, ys_out;
+
+    // packing and unpacking should be inverse operations
+    unpack_xs_ys_fixed12_4(pack_xs_ys_fixed12_4(xs, ys), &xs_out, &ys_out);
+    assert(xs_out == xs);
+    assert(ys_out == ys);
 }
